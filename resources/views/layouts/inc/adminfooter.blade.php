eb046a2a7e0b0df0fadb6d9d0095a12c4bae3666
Sababa Rahman <sababa.rahman.meem@g.bracu.ac.bd>
2022-12-20 01:42:56 +0600

    Slight Modification

--- resources/views/layouts/inc/adminfooter.blade.php
+++ resources/views/layouts/inc/adminfooter.blade.php
@@ -4,7 +4,7 @@
             <ul>
               <li>
                 <a href="https://www.youtube.com/watch?v=ngWZaSbw130">
-                  Why should you rent?
+                  Why us?
                 </a>
               </li>
               <li>
@@ -14,19 +14,19 @@
               </li>
               <li>
                 <a href="http://blog.creative-tim.com">
-                  
+
                 </a>
               </li>
               <li>
                 <a href="https://www.creative-tim.com/license">
-                  
+
                 </a>
               </li>
             </ul>
           </nav>
           <div class="copyright float-right" id="date">
-            Developed by
-            <a href="https://www.linkedin.com/in/kazi-md-al-wakil-60314a193/" target="_blank">Kazi Md. Al-Wakil</a>
+            Developed by Sababa Rahman
+
           </div>
         </div>
       </footer>
